95f19b20032f8a3d42fb6e97e399a0fdd096c5ed
Rich <richardwjones@hotmail.com>
2022-01-27 12:14:38 +1300

    Initial Commit

---
+++ TradeMe_Categories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TradeMe_Categories
+//
+//  Created by Rich on 1/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TradeMe_Categories/TradeMe_CategoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradeMe_CategoriesApp.swift
+//  TradeMe_Categories
+//
+//  Created by Rich on 1/27/22.
+//
+
+import SwiftUI
+
+@main
+struct TradeMe_CategoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
