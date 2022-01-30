19210ff2c468f3d7ac969c3bba020ed3deca2a6c
Rich <richardwjones@hotmail.com>
2022-01-31 11:20:22 +1300

    Changed name of OAuthHelper U/Tests

--- TradeMe_CategoriesTests/OAuthHelperTests.swift
+++ TradeMe_CategoriesTests/OAuthHelperTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import TradeMe_Categories
 
-class TradeMe_CategoriesTests: XCTestCase {
+class OAuthHelperTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
